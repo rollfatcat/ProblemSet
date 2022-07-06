a5e17e2ed410b985ea8a23d69f92ef1556d3a386
rollfatcat <crhsieh1114@outlook.com>
2022-07-07 00:01:02 +0800

    update

--- 演算法(高等)/BinaryIndexTree/e406.cc
+++ 演算法(高等)/BinaryIndexTree/e406.cc
@@ -1,51 +1,57 @@
+/* 題目給定Ａ陣列的前ｋ個數字和，透過 gen_dat() 產生完整測資的Ｎ個數字和Ｑ個操作
+ * 操作分為(1)０ｘｙ代表把ｘ這格位置的數字改為ｙ (2)１ｘｙ代表Ａ陣列中位置ｘ到位置ｙ的總和
+ * 題目要求：單點更新和區間總和查詢
+ */
 #include<bits/stdc++.h>
 using namespace std;
- 
-const int MaxN=1000000+5;
-const int MaxQ=100000+5;
- 
+
+const int MaxN=1e6+5;
+const int MaxQ=1e5+5;
 int BIT[MaxN]={};
 int A[MaxN];
+bool C[MaxQ];
 int X[MaxQ];
 int Y[MaxQ];
-bool C[MaxQ];
- 
 int k, m, N, Q;
-inline int lowbit(int x){ return x&-x; }
-inline void gen_dat(){
-  for(int i=k+1; i<=max(Q,N); i++)
-    A[i]=( A[i-2]*97+A[i-1]*3 )%m+1;
-  for(int i=1,j=max(Q,N); i<=Q; i++,j--){
-    C[i]=A[i]&1;
-    X[i]=(A[i]+A[j])%N+1;
-    Y[i]=(C[i])? X[i]+( (A[i]<<3)+(A[j]<<5)+m )%( N-X[i]+1 ):
-    						     +( (A[i]<<3)+(A[j]<<5) )%m+1;
-  }
+
+void gen_dat(){
+	for(int i=k+1; i<=max(Q,N); i++ )
+		A[i]=( A[i-2]*97 + A[i-1]*3 )%m+1;
+	for(int i=1,j=max(Q,N); i<=Q; i++,j--){
+		C[i]= A[i]&1;
+		X[i]=(A[i]+A[j])%N+1;
+		Y[i]=(C[i])? X[i]+( (A[i]<<3)+(A[j]<<5)+m )%( N-X[i]+1 ): ((A[i]<<3)+(A[j]<<5))%m+1;
+	}
 }
-inline int QuerySum(int pos,int out=0){
-	for(int idx=pos;idx>0;idx-=lowbit(idx))
-		out+=BIT[idx];
-	return out;
+// 二進位的ｘ最後一個是１的位元
+int lowbit(int x){ return x&-x; }
+int Query(int p,int ret=0){
+	for(; 0<p; p-=lowbit(p))
+		ret+= BIT[p];
+	return ret;
+}
+void Update(int p,int v){
+	for(A[p]+=v; p<=N; p+=lowbit(p))
+		BIT[p]+=v;
 }
 int main(){
-	scanf("%d %d",&k,&m);
-	for(int i=1;i<=k;i++)
-		scanf("%d",&A[i]);
-	scanf("%d %d",&N,&Q);
+	cin>>k>>m;
+	for(int i=1; i<=k; i++)
+		cin>>A[i];
+	cin>>N>>Q;
 	gen_dat();
-	// BuildTree();
-	for(int i=1;i<=N;i++)
-		for(int x=i;x<=N;x+=lowbit(x))
-			BIT[x]+=A[i];
-	//
-	for(int Qid=1;Qid<=Q;Qid++){
-		if(C[Qid]){
-			printf("%d\n",(QuerySum(Y[Qid])-QuerySum(X[Qid]-1))%m);
+	// Build Tree
+	for(int n=1; n<=N; n++){
+		Update(n,A[n]);
+		A[n]>>=1;
+	}
+	// 根據要求操作
+	for(int q=1; q<=Q; q++){
+		if( C[q]==0 ){
+			Update(X[q],Y[q]-A[X[q]]);
 		}else{
-			int diff=Y[Qid]-A[X[Qid]];
-			for(int x=X[Qid];x<=N;x+=lowbit(x))
-				BIT[x]+=diff;
-			A[X[Qid]]+=diff;
+			if(X[q]>Y[q]) swap(X[q],Y[q]);
+			cout<< ( Query(Y[q])-Query(X[q]-1) )%m <<'\n';
 		}
 	}
 }
