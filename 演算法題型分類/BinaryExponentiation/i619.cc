f9c76320e8812304b73ac348e8e34e8a55e08a10
rollfatcat <crhsieh1114@outlook.com>
2023-03-22 03:09:32 +0800

    update

--- 演算法題型分類/BinaryExponentiation/i619.cc
+++ 演算法題型分類/BinaryExponentiation/i619.cc
@@ -1,4 +1,4 @@
-/* 給定Ａ,Ｐ,Ｍ，問 sum(pow(x,i))%M, i=0,1,2...P-1
+/* 給定Ａ,Ｐ,Ｍ，問 sum(pow(x,i))%M, i=0,1,2...P
  * 盲點：轉換為等比公式時需要做除法，除法在取模時會轉為乘法反元素但是這必須符合前提：Ａ-1和Ｍ必須互質
  * 核心：透過矩陣乘法 和 快速冪次法計算
  *      矩陣 = [[A,1],[0,1]]*[0,1]，
@@ -6,9 +6,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MaxA=1e9;
-const int MaxM=1e9;
-const long MaxP=1e12;
+const int MaxA=2e9;
+const int MaxM=2e9;
+const long MaxP=1e18;
 
 int A, M;
 long P;
@@ -28,17 +28,23 @@ struct MAT{
 	ret.v[1][1]=(v[1][0]*rhs.v[0][1]+v[1][1]*rhs.v[1][1])%M;
 		return ret;
 	}
-} now;
+};
 
 int main(){
-	// input
-	cin>>now.v[0][0]>>P>>M;
-	// Fast Exponentiation
-	vector<long> vec={0,1};
-	for( ; 0<P; P>>=1){
-		if( P&1 )
-			vec=now.mul_vec(vec);
-		now=now*now;
+	int caseT;
+	
+	scanf("%d",&caseT);
+	while( caseT-->0 ){
+		// input
+		MAT now;
+		scanf("%ld %ld %d",&now.v[0][0],&P,&M);
+		// Fast Multiply
+		vector<long> vec={0,1};
+		for(P+=1 ; 0<P; P>>=1){
+			if( P&1 )
+				vec=now.mul_vec(vec);
+			now=now*now;
+		}
+		printf("%ld\n",vec[0]);
 	}
-	cout<<vec[0];
 }
